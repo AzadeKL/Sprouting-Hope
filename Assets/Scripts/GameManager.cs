3939cc29c84309c4fda3c8c1970698a003323a97
lkaldes <lkaldes01@gmail.com>
2024-07-17 20:49:57 -0700

    started tilling and planting code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,11 @@ using UnityEngine.Tilemaps;
 public class GameManager : MonoBehaviour
 {
     
-    [SerializeField] private Tilemap map;
+    [SerializeField] private Tilemap farmLand;
+    [SerializeField] private Tilemap farmPlants;
+    [SerializeField] private Tilemap buildings;
+
+    public Tile sprite;
     public GameObject player;
 
 
@@ -33,16 +37,36 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.E))
+        {
+            player.GetComponent<PlayerInventory>().hand = "hoe";
+            Debug.Log("Now equipped " + player.GetComponent<PlayerInventory>().hand);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            player.GetComponent<PlayerInventory>().hand = "wheat seeds";
+            Debug.Log("Now equipped " + player.GetComponent<PlayerInventory>().hand);
+        }
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3Int gridPosition = map.WorldToCell(player.transform.position);
-            TileBase clickedTile = map.GetTile(gridPosition);
-            if (clickedTile)
+            Vector3Int gridPosition = farmLand.WorldToCell(player.transform.position);
+            TileBase clickedTile = farmLand.GetTile(gridPosition);
+            if (clickedTile) switch(player.GetComponent<PlayerInventory>().hand)
             {
-                tileState[clickedTile]++;
-                int newState = tileState[clickedTile];
-                Debug.Log("Dirt space set to " + newState);
+                case "hoe":
+                    tileState[clickedTile] = 1;
+                    Debug.Log("Dirt space set to " +  tileState[clickedTile]);
+                    break;
+                case "wheat seeds":
+                    if (tileState[clickedTile] == 1)
+                    {
+                        farmPlants.SetTile(gridPosition, sprite);
+                        //plantTile.GetTileData.sprite = sprite;
+                        Debug.Log("Planted wheat seeds");
+                    }
+                    break;
             }
+            
         }
     }
 
