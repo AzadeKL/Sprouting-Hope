9c96e32ee7c11b63626b2b5fdd21aa8eb8b0a318
Xentios <xenq@hotmail.com>
2024-07-19 14:53:13 +0300

    Added Day Night Cycle
    It is very simple but we may extend in future

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DayNightCycle : MonoBehaviour
+{
+
+    [SerializeField] private Light2D globalLight;
+    [SerializeField] private float dayDuration = 60f;
+    [SerializeField] private float nightIntensity = 0.1f;
+    [SerializeField] private float dayIntensity = 1f;
+    [SerializeField] private Color dayColor = Color.white;
+    [SerializeField] private Color nightColor = Color.blue;
+    [Range(0f, 60f)]
+    [SerializeField] private float cycleStartTime;
+
+    private float cycleTimer;
+
+    void Start()
+    {
+        if (globalLight == null)
+        {
+            globalLight = GetComponent<Light2D>();
+        }
+        cycleTimer = cycleStartTime;
+    }
+
+    void Update()
+    {
+        cycleTimer += Time.deltaTime;
+        float cycleProgress = cycleTimer / dayDuration;
+        float lightIntensity;
+        Color lightColor;
+
+        if (cycleProgress < 0.5f)
+        {
+            lightIntensity = Mathf.Lerp(nightIntensity, dayIntensity, cycleProgress * 2);
+            lightColor = Color.Lerp(nightColor, dayColor, cycleProgress * 2);
+        }
+        else
+        {
+            lightIntensity = Mathf.Lerp(dayIntensity, nightIntensity, (cycleProgress - 0.5f) * 2);
+            lightColor = Color.Lerp(dayColor, nightColor, (cycleProgress - 0.5f) * 2);
+        }
+
+        globalLight.intensity = lightIntensity;
+        globalLight.color = lightColor;
+
+        if (cycleProgress >= 1f)
+        {
+            cycleTimer = 0f;
+        }
+    }
+}
