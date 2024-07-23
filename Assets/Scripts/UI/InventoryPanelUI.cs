2b857f003f3c2544ae248719ddeeb02a2f6df35e
Xentios <xenq@hotmail.com>
2024-07-23 23:01:06 +0300

    Added Animations for UI
    reduced column count

--- Assets/Scripts/UI/InventoryPanelUI.cs
+++ Assets/Scripts/UI/InventoryPanelUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,10 +7,11 @@ public class InventoryPanelUI : MonoBehaviour
 {
 
     private List<Transform> tabPanels;
+    private List<RectTransform> buttonsRectTr;
 
     private void Awake()
     {
-
+        buttonsRectTr = new();
 
     }
 
@@ -25,17 +27,16 @@ public class InventoryPanelUI : MonoBehaviour
 
     private void OnEnable()
     {
-        var buttons = transform.GetChild(0).GetComponentsInChildren<Button>();
-        foreach (var button in buttons)
+        foreach (Transform tr in transform.GetChild(0))
         {
-            button.onClick.AddListener(() => ButtonClicks(button));
+            buttonsRectTr.Add(tr.GetComponentInChildren<Button>().GetComponent<RectTransform>());
+            tr.GetComponentInChildren<Button>().onClick.AddListener(() => ButtonClicks(tr.GetComponentInChildren<Button>()));
         }
-
     }
 
     private void OnDisable()
     {
-        var buttons = transform.GetChild(0).gameObject.GetComponentsInChildren<Button>();
+        var buttons = transform.GetChild(0).GetChild(0).gameObject.GetComponentsInChildren<Button>();
         foreach (var button in buttons)
         {
             button.onClick.RemoveAllListeners();
@@ -45,12 +46,18 @@ public class InventoryPanelUI : MonoBehaviour
 
     private void ButtonClicks(Selectable selectable)
     {
-        var index = selectable.transform.GetSiblingIndex();
+        var index = selectable.transform.parent.GetSiblingIndex();
         foreach (var tab in tabPanels)
         {
             tab.gameObject.SetActive(false);
+            // tab.GetComponent<RectTransform>().DOAnchorPosY
+        }
+        foreach (var rectTransform in buttonsRectTr)
+        {
+            rectTransform.DOAnchorPosY(-25, 1f).SetEase(Ease.OutBounce);
         }
         tabPanels[index].gameObject.SetActive(true);
+        buttonsRectTr[index].DOAnchorPosY(0, 1f).SetEase(Ease.OutCubic);
     }
 
 }
