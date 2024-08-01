c1b99893ebb2d3ac8c25bfc6a01be6f93dcfc971
Xentios <xenq@hotmail.com>
2024-08-01 19:05:45 +0300

    Binded seed and crop collection to item quality

--- Assets/Scripts/SeedFactory.cs
+++ Assets/Scripts/SeedFactory.cs
@@ -13,13 +13,13 @@ public class SeedFactory : MonoBehaviour
 
     }
 
-
+    private int itemModifier = 0;
 
     public void CreateSeed(Vector3 pos)
     {
         if (seeds.Count == 0) return;
 
-        var seedCount = Random.Range(1, Mathf.Max(2, seedRandomSpawnUpperLimit));
+        var seedCount = Random.Range(1, Mathf.Max(2, seedRandomSpawnUpperLimit + itemModifier));
 
         for (int i = 0; i < seedCount; i++)
         {
@@ -29,7 +29,7 @@ public class SeedFactory : MonoBehaviour
 
     }
 
-    
+
     public void CreateCrop(Vector3 pos, string crop)
     {
         if (crops.Count == 0) return;
@@ -38,31 +38,54 @@ public class SeedFactory : MonoBehaviour
         switch (crop)
         {
             case "Wheat":
-                cropCount = Random.Range(1, 2);
+            cropCount = Random.Range(1, 2 + itemModifier);
 
-                for (int i = 0; i < cropCount; i++)
-                {
-                    Instantiate(crops[0], pos, Quaternion.identity);
-                }
-                break;
+            for (int i = 0; i < cropCount; i++)
+            {
+                Instantiate(crops[0], pos, Quaternion.identity);
+            }
+            break;
             case "Tomato":
-                cropCount = Random.Range(2, 4);
+            cropCount = Random.Range(2, 4 + itemModifier);
 
-                for (int i = 0; i < cropCount; i++)
-                {
-                    Instantiate(crops[1], pos, Quaternion.identity);
-                }
-                break;
+            for (int i = 0; i < cropCount; i++)
+            {
+                Instantiate(crops[1], pos, Quaternion.identity);
+            }
+            break;
             case "Lentil":
-                cropCount = Random.Range(1, 3);
+            cropCount = Random.Range(1, 3 + itemModifier);
 
-                for (int i = 0; i < cropCount; i++)
-                {
-                    Instantiate(crops[2], pos, Quaternion.identity);
-                }
-                break;
+            for (int i = 0; i < cropCount; i++)
+            {
+                Instantiate(crops[2], pos, Quaternion.identity);
+            }
+            break;
         }
 
+    }
+
+    public void HandleHandChange(GameObject item)
+    {
+        if (item == null)
+        {
+            itemModifier = 0;
+        }
+        else
+        {
+            var name = item.GetComponent<InventoryIcon>().item;
+            //Debugger.Log("Name im HandChange " + name, Debugger.PriorityLevel.High);
+            int result = name switch
+            {
+                string a when a.Contains("Rusty") => 1,
+                string b when b.Contains("Bronze") => 2,
+                string b when b.Contains("Silver") => 3,
+                string b when b.Contains("Gold") => 10,
+                _ => 0
+            };
+            itemModifier = result;
+            //Debugger.Log("Result of HandChange " + result, Debugger.PriorityLevel.High);
+        }
     }
 
 }
