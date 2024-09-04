9242fc535fd579d1192d92aff48aced434c9b5f4
Sgoel-exe <sgoel@email.arizona.edu>
2024-09-04 15:48:14 -0400

    Complete Smart Tutorial dialouge system

--- Assets/Scripts/UI/TutorialPopUp.cs
+++ Assets/Scripts/UI/TutorialPopUp.cs
@@ -1,6 +1,7 @@
-using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialPopUp : MonoBehaviour
 {
@@ -8,20 +9,23 @@ public class TutorialPopUp : MonoBehaviour
     [Header("Tutorial Text")]
     [SerializeField, Tooltip("Do not set in Inspector, only to show what will be displayed")] 
     private string tutorialText;
+    private string[] tutorialTextList;
     private bool isVisiable = false;
 
     [Header("Tutorial PopUp References")]
     public GameObject tutorialPopUp;
     public Image backgroundImage;
-    public TextMeshProUGUI tutorialTextObject;
+    public TextMeshProUGUI textObject;
 
     [Header("Tutorial PopUp Settings")]
     public Vector2 popUpPadding = new Vector2(40, 20);
+    public float maxWidth = 500;
 
     // Start is called before the first frame update
     void Start()
     {
         setVisibility(false);
+        textObject.SetText("");
     }
 
     // Update is called once per frame
@@ -33,6 +37,8 @@ public class TutorialPopUp : MonoBehaviour
     public void setText(string text)
     {
         tutorialText = text;
+        tutorialTextList = tutorialText.Split(' ');
+        Debug.Log("Tutorial Text list: " + tutorialTextList.Length);
     }
 
     private void setVisibility(bool visibility)
@@ -43,9 +49,38 @@ public class TutorialPopUp : MonoBehaviour
 
     private void setDimensionsforText()
     {
-        tutorialTextObject.SetText(tutorialText);
-        tutorialTextObject.ForceMeshUpdate();
-        Vector2 textSize = tutorialTextObject.GetRenderedValues(false);
+        Vector2 textSize = textObject.GetRenderedValues(false);
+        while(true)
+        {
+            tutorialText = tutorialText.Trim();
+            textObject.SetText(tutorialText);
+            textObject.ForceMeshUpdate();
+            textSize = textObject.GetRenderedValues(false);
+            if(textSize.x < maxWidth)
+            {
+                break;
+            }
+            else
+            {
+                List<string> tutorialTextListTemp = new List<string>();
+                string tempString = "";
+                for (int i = 0; i < tutorialTextList.Length; i++)
+                {
+                    if (textObject.GetPreferredValues(tempString + " " + tutorialTextList[i]).x < maxWidth)
+                    {
+                        tempString += " " + tutorialTextList[i];
+                    }
+                    else
+                    {
+                        tutorialTextListTemp.Add(tempString);
+                        tempString = tutorialTextList[i];
+                    }
+                }
+                tutorialTextListTemp.Add(tempString);
+                tutorialText = string.Join("\n", tutorialTextListTemp);
+            }
+        }
+        
         
         setBox(textSize);
     }
@@ -53,8 +88,8 @@ public class TutorialPopUp : MonoBehaviour
     private void setBox(Vector2 size)
     {
         tutorialPopUp.GetComponent<RectTransform>().sizeDelta = size + popUpPadding;
-        Vector2 tutorialPosition = new Vector2(-(size.x / 2), size.y / 2);
-        tutorialTextObject.rectTransform.localPosition = tutorialPosition;
+        Vector2 tutorialPosition = new Vector2(-size.x / 2, -size.y / 2);
+        textObject.rectTransform.localPosition = tutorialPosition * new Vector2(2, 1);
         tutorialPopUp.GetComponent<RectTransform>().localPosition = tutorialPosition;
     }
 
@@ -67,8 +102,8 @@ public class TutorialPopUp : MonoBehaviour
 
     public void hidePopUp()
     {
-        tutorialTextObject.SetText("");
-        tutorialTextObject.ForceMeshUpdate();
+        textObject.SetText("");
+        textObject.ForceMeshUpdate();
         // setDimensionsforText();
         setVisibility(false);
     }
