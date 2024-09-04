1ed742139033dcd1a2ec2d45cfa0485dfad10d49
Sgoel-exe <sgoel@email.arizona.edu>
2024-09-04 15:48:14 -0400

    Complete Smart Tutorial dialouge system

--- Assets/Scripts/UI/TutorialTile.cs
+++ Assets/Scripts/UI/TutorialTile.cs
@@ -3,49 +3,53 @@ using UnityEngine.Events;
 
 
 public class TutorialTile : MonoBehaviour
+{
+    [Header("Tutorial Text")]
+    [Multiline,Tooltip("Enter what tutorial should say")]public string tutorialText;
+
+    public UnityEvent<string> tutorialEvent;
+    public UnityEvent tutorialEventClose;
+
+    [Header("Destroy Settings")]
+    public bool shouldBeDestroyed = false;
+    [Tooltip("Only works when the bool is true")] public float destroyTime = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        [Header("Tutorial Text")]
-        [Multiline,Tooltip("Enter what tutorial should say")]public string tutorialText;
+        tutorialEvent.AddListener(GameObject.FindGameObjectWithTag("TutorialPopUp").GetComponent<TutorialPopUp>().showPopUp);
+        tutorialEventClose.AddListener(GameObject.FindGameObjectWithTag("TutorialPopUp").GetComponent<TutorialPopUp>().hidePopUp);
+    }
 
-        public UnityEvent<string> tutorialEvent;
-        public UnityEvent tutorialEventClose;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
-        [Header("Destroy Settings")]
-        public bool shouldBeDestroyed = false;
-        [Tooltip("Only works when the bool is true")] public float destroyTime = 0.4f;
-
-        // Start is called before the first frame update
-        void Start()
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            tutorialEvent.AddListener(GameObject.FindGameObjectWithTag("TutorialPopUp").GetComponent<TutorialPopUp>().showPopUp);
-            tutorialEventClose.AddListener(GameObject.FindGameObjectWithTag("TutorialPopUp").GetComponent<TutorialPopUp>().hidePopUp);
+            tutorialEvent.Invoke(tutorialText);  
         }
+    }
 
-        // Update is called once per frame
-        void Update()
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            
-        }
-
-        void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.gameObject.CompareTag("Player"))
+            tutorialEventClose.Invoke();  
+            if (shouldBeDestroyed)
             {
-                Debug.Log("Tutorial Tile Triggered");
-                tutorialEvent.Invoke(tutorialText);  
-                // Destroy(gameObject, 0.1f);
-            }
-        }
-
-        void OnTriggerExit2D(Collider2D other)
-        {
-            if (other.gameObject.CompareTag("Player"))
-            {
-                tutorialEventClose.Invoke();  
-                if (shouldBeDestroyed)
-                {
-                    Destroy(gameObject, destroyTime);
-                }
+                Destroy(gameObject, destroyTime);
             }
         }
     }
+
+    private void OnDestroy()
+    {
+        tutorialEvent.RemoveListener(GameObject.FindGameObjectWithTag("TutorialPopUp").GetComponent<TutorialPopUp>().showPopUp);
+        tutorialEventClose.RemoveListener(GameObject.FindGameObjectWithTag("TutorialPopUp").GetComponent<TutorialPopUp>().hidePopUp);
+    }
+}
